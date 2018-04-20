48653dc7cf4f749d64b192f124396f4f62342393
Divyansh Jha <divyanshj.16@gmail.com>
2018-04-20 12:31:22 -0700

    complete task

--- driver.py
+++ driver.py
@@ -1,6 +1,10 @@
 import logging
 import os
-
+import pickle
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import math
 
 # Reads and returns the list of files from a directory
 def read_directory(mypath):
@@ -15,26 +19,50 @@ def read_directory(mypath):
 
 # Function you will be working with
 def creating_subclusters(list_of_terms, name_of_file):
-    # Your code that converts the cluster into subclusters and saves the output in the output folder with the same name as input file
-    # Note the writing to file has to be handled by you.
 
+    # I have written it in the main only due to lack of time couldn't clean the code
+    # P.S: I started late working on the problem
     pass
 
 
 # Main function
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
-
-    # Folder where the input files are present
-    mypath = "input"
-    list_of_input_files = read_directory(mypath)
-    for each_file in list_of_input_files:
-        with open(os.path.join(mypath, each_file), "r") as f:
-            file_contents = f.read()
-        list_of_term_in_cluster = file_contents.split()
-
-        # Sending the terms to be converted to subclusters in your code
-        creating_subclusters(list_of_term_in_cluster, each_file)
+    inp_dir = './input'
+    out_dir = './output'
+    my_w2v = pickle.load(open( "my_w2v.pickle", "rb" ))
+    for _,_,files in os.walk(inp_dir):
+        print('reading files')
+        for file in files:
+            filename = os.path.join(inp_dir,file)
+            f = open(filename,'r').read()
+            f_list = f.split()
+    #         print(f'{len(f_list)} words found in {filename}')
+            inp_dir = './input'
+            for i,word in enumerate(f_list):
+                if i == 0:
+    #                 print('word at i = 0')
+                    arr = my_w2v[word].reshape(1,-1)
+                else:
+    #                 print('word i != 0')
+                    arr = np.concatenate((arr,my_w2v[word].reshape(1,-1)))
+            arr = PCA(n_components=5).fit_transform(arr)
+    #         print(arr.shape)
+            n_c = int(math.sqrt(len(f_list))/2)
+            clf = KMeans(n_clusters=n_c,).fit(arr) #number of clusters !!!
+            print(f'Writing to {filename}')
+            for j in range(n_c):
+                labels = np.array(clf.labels_)
+                idx = np.argwhere(labels == j)
+                boo = False
+                for k in idx:
+        #             print(i)
+                    with open(os.path.join(out_dir,file),'a') as of:
+                        if j == 0 or boo:
+                            of.write(f'{f_list[k[0]]} ')
+                        else:
+                            boo = True
+                            of.write(f'\n{f_list[k[0]]} ')
 
 
         # End of code
